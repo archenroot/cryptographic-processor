d2f078532e72654674baaab70c18fc3633769ed6
archenroot <archenroot@gmail.com>
2015-09-24 13:58:32 +0100

    Addedd documentation.

--- src/main/java/org/prometheus/cryptographic_processor/pgp/PubringDump.java
+++ src/main/java/org/prometheus/cryptographic_processor/pgp/PubringDump.java
@@ -1,5 +1,6 @@
 package org.prometheus.cryptographic_processor.pgp;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.Security;
@@ -21,6 +22,7 @@ import org.bouncycastle.util.encoders.Hex;
  * Basic class which just lists the contents of the public key file passed
  * as an argument. If the file contains more than one "key ring" they are
  * listed in the order found.
+ * @since 1.0 
  */
 public class PubringDump 
 {
@@ -30,6 +32,11 @@ public class PubringDump
     
     private static final Logger LOGGER = LogManager.getLogger();
     
+    /**
+     * Gets String-like algorithm used in the key.
+     * @param algId integer representation of algorithm.
+     * @return String representation of algorithm.
+     */
     public static String getAlgorithm(
         int    algId)
     {
@@ -54,20 +61,26 @@ public class PubringDump
         case PublicKeyAlgorithmTags.DIFFIE_HELLMAN:
             return "DIFFIE_HELLMAN";
         }
-
-        return "unknown";
+        return "UNKNOWN";
     }
-    /* Method to print out Public key ring collection
+    
+    /**
+     * Method to print out Public key ring collection
      * No parameter indicates that this function will try to locate default
      * pubring by itslef.
-    */
+     */
     public static void printPubringDump(){
         // TODO
     }
     
-    public static void printPubringDump(String pubringFile) throws PGPProcessingException{
-    
-        try {
+    /**
+     * Dumps information from provided PGP public key ring file.
+     * @param pubringFile public key ring file full path
+     * @throws PGPProcessingException PGP processing wrapper exception
+     */
+    public static void printPubringDump(File pubringFile) throws PGPProcessingException{
+         try {
+            
             //String pubring = "c:\\cygwin64\\home\\ljech\\.gnupg\\pubring.gpg";
             //String pubringFile = "c:\\cygwin64\\home\\ljech\\.gnupg\\pubring.gpg";
             //
@@ -92,9 +105,10 @@ public class PubringDump
                 {
                     pgpPub.getPublicKey();
                 }
-                catch (Exception e)
+                catch (Exception ex)
                 {
-                    e.printStackTrace();
+                    String msg = "TODO";
+                    LOGGER.error(msg,ex);
                     continue;
                 }
                 
@@ -120,7 +134,7 @@ public class PubringDump
                     }
 
                     LOGGER.info("            Algorithm: " + getAlgorithm(pgpKey.getAlgorithm()));
-                    LOGGER.info("            Fingerprint: " + new String(Hex.encode(pgpKey.getFingerprint())));
+                    LOGGER.info("            Fingerprint: " + new String(Hex.encode(pgpKey.getFingerprint()),"UTF-8"));
                     LOGGER.info("            Valid Seconds: " + pgpKey.getValidSeconds());
                 }
             }
@@ -134,59 +148,12 @@ public class PubringDump
             throw new PGPProcessingException(msg,ex);
         }
     }
-    public static void main(String[] args)
-        throws Exception
-    {
-        
-        String pubring = "c:\\cygwin64\\home\\ljech\\.gnupg\\pubring.gpg";
-        //String pubring = "c:\\cygwin64\\home\\ljech\\.gnupg\\pubring.gpg";
-        //
-        // Read the public key rings
-        //
-        PGPPublicKeyRingCollection    pubRings = new PGPPublicKeyRingCollection(
-            PGPUtil.getDecoderStream(new FileInputStream(pubring)), new JcaKeyFingerprintCalculator());
-
-        Iterator    rIt = pubRings.getKeyRings();
-            
-        while (rIt.hasNext())
-        {
-            PGPPublicKeyRing    pgpPub = (PGPPublicKeyRing)rIt.next();
-            
-            try
-            {
-                pgpPub.getPublicKey();
-            }
-            catch (Exception e)
-            {
-                e.printStackTrace();
-                continue;
-            }
-
-            Iterator    it = pgpPub.getPublicKeys();
-            boolean     first = true;
-            while (it.hasNext())
-            {
-                PGPPublicKey    pgpKey = (PGPPublicKey)it.next();
-                Iterator userIdIterator = pgpKey.getUserIDs();
-                while (userIdIterator.hasNext()){
-                        LOGGER.info("User ID: " + userIdIterator.next());
-                }
-                
-                if (first)
-                {
-                    LOGGER.info("Key ID: " + Long.toHexString(pgpKey.getKeyID()));
-                    
-                    first = false;
-                }
-                else
-                {
-                    LOGGER.info("Key ID: " + Long.toHexString(pgpKey.getKeyID()) + " (subkey)");
-                }
-                
-                LOGGER.info("            Algorithm: " + getAlgorithm(pgpKey.getAlgorithm()));
-                LOGGER.info("            Fingerprint: " + new String(Hex.encode(pgpKey.getFingerprint())));
-                LOGGER.info("            Valid Seconds: " + pgpKey.getValidSeconds());
-            }
-        }
+    /**
+     * Dumps information from provided PGP public key ring file.
+     * @param pubringFile public key ring file full path.
+     * @throws PGPProcessingException PGP processing wrapper exception.
+     */
+    public static void printPubringDump(String pubringFile) throws PGPProcessingException{
+        printPubringDump(new File(pubringFile));
     }
 }
