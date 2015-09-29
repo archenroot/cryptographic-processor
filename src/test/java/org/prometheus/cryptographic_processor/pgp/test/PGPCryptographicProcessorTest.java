19c6dc04134320e491ae37eafc274a2cdd992640
archenroot <archenroot@gmail.com>
2015-09-29 12:42:47 +0100

    Finished initial PGP unit test.

--- src/test/java/org/prometheus/cryptographic_processor/pgp/test/PGPCryptographicProcessorTest.java
+++ src/test/java/org/prometheus/cryptographic_processor/pgp/test/PGPCryptographicProcessorTest.java
@@ -1,15 +1,16 @@
 package org.prometheus.cryptographic_processor.pgp.test;
 
+import java.io.File;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
-import org.prometheus.cryptographic_processor.CryptographicProcessor;
+import org.junit.rules.ErrorCollector;
 import org.prometheus.cryptographic_processor.CryptographicProcessorException;
 import org.prometheus.cryptographic_processor.CryptographicProcessorFactory;
 import org.prometheus.cryptographic_processor.CryptographicProcessorType;
-import org.prometheus.cryptographic_processor.pgp.KeyBasedLargeFileProcessor;
 import org.prometheus.cryptographic_processor.pgp.PGPCryptographicProcessor;
 
 /**
@@ -17,50 +18,86 @@ import org.prometheus.cryptographic_processor.pgp.PGPCryptographicProcessor;
  * @author Ladislav Jech <archenroot at gmail.com>
  */
 public class PGPCryptographicProcessorTest {
-    
+
+    @Rule
+    public ErrorCollector collector;
+
+    /**
+     * PGP Encryption related test variables.
+     */
+    private final String encInputFileName = "c:\\cygwin64\\home\\ljech\\SCEE_TR0000208-090-CY_20150428.txt.zip";
+    private final String encOutputFileName = "c:\\cygwin64\\home\\ljech\\SCEE_TR0000208-090-CY_20150428.txt.zip.pgp";
+    private final String encPublicKeyFileName = "c:\\cygwin64\\home\\ljech\\.gnupg\\fma2_public_key.asc";
+    private final boolean encArmor = false;
+    private final boolean encWithIntegrityCheck = true;
+
+    /**
+     * PGP Encryption related test variables.
+     */
+    private final String decInputFileName = "c:\\cygwin64\\home\\ljech\\SCEE_TR0000208-090-CY_20150428.txt.zip.pgp";
+    private final String decPrivateKeyFileName = "c:\\cygwin64\\home\\ljech\\.gnupg\\fma2_private_key.asc";
+    private final char[] decPasswd = "Sony123".toCharArray();
+    private final String decDefaultFileName = "c:\\cygwin64\\home\\ljech\\SCEE_TR0000208-090-CY_20150428.txt-decrypted.zip";
+
     public PGPCryptographicProcessorTest() {
+        this.collector = new ErrorCollector();
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
-       
+
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
-        
+
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
-    
+
     public void tearDown() {
     }
 
-    @Test(expected = CryptographicProcessorException.class)
-    public void testException() throws CryptographicProcessorException{
-        CryptographicProcessor pgpgCP = null;
-            pgpgCP = CryptographicProcessorFactory.buildCryptographicProcessor(CryptographicProcessorType.AES);
-    }
-   
+    /*
+     @Test(expected = CryptographicProcessorException.class)
+     public void testException() throws CryptographicProcessorException{
+     CryptographicProcessor pgpgCP = null;
+     pgpgCP = CryptographicProcessorFactory.buildCryptographicProcessor(CryptographicProcessorType.AES);
+     }
+     */
     @Test
-    public void testPGPEncryptFile() throws CryptographicProcessorException{
-        PGPCryptographicProcessor pgpgCP = null;
+    public void testPGPEncryptFile() throws CryptographicProcessorException {
+        PGPCryptographicProcessor pgpgCP;
         pgpgCP = (PGPCryptographicProcessor) CryptographicProcessorFactory.buildCryptographicProcessor(CryptographicProcessorType.PGP);
-        pgpgCP.encryptFileWithKey("c:\\cygwin64\\home\\ljech\\SCEE_TR0000208-090-CY_20150428.txt.zip.pgp",
-                "c:\\cygwin64\\home\\ljech\\SCEE_TR0000208-090-CY_20150428.txt.zip",
-                "c:\\cygwin64\\home\\ljech\\.gnupg\\fma2_public_key.asc",
-                false,
-                true);
+
+        pgpgCP.encryptFileWithKey(
+                this.encInputFileName,
+                this.encOutputFileName,
+                this.encPublicKeyFileName,
+                this.encArmor,
+                this.encWithIntegrityCheck
+        );
+
+        if (new File(this.encOutputFileName).exists()) {
+            throw new CryptographicProcessorException(
+                    "Expected encrypted file "
+                    + this.encOutputFileName
+                    + " doesn't exist.");
+        }
     }
-    
+
     @Test
-    public void testPGPDecryptFile(){
-        
+    public void testPGPDecryptFile() throws CryptographicProcessorException {
+        PGPCryptographicProcessor pgpgCP;
+        pgpgCP = (PGPCryptographicProcessor) CryptographicProcessorFactory.buildCryptographicProcessor(CryptographicProcessorType.PGP);
+        pgpgCP.decryptFileWithKey(
+                this.decInputFileName,
+                this.decPrivateKeyFileName,
+                this.decPasswd,
+                this.decDefaultFileName);
     }
-    
-  
 }
